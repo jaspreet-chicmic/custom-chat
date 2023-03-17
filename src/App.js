e17560b245d7df0bbe58f519315a16356c9bf30a
jaspreet-chicmic <jaspreet.singh@chicmic.co.in>
2023-03-17 18:58:19 +0530

    js file upload

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import "react-chat-elements/dist/main.css"
 import { MessageBox,ChatItem } from "react-chat-elements"
 import logo from './logo.svg';
 import './App.css';
+import ChatSideBar from "./Components";
 function App() {
   return (
     <div className="App">
+      <ChatSideBar/>
+      
       <MessageBox
         position={"left"}
         type={"text"}
         title={"Message Box Title"}
         text="Here is a text type message box"
       />
+      
       
       <ChatItem
         avatar={'https://facebook.github.io/react/img/logo.svg'}
@@ -19,7 +23,16 @@ function App() {
         subtitle={'What are you doing?'}
         date={new Date()}
         unread={0}
+        onDragOver={console.log(90)}
       />
+
+
+    <MessageBox
+      position={"left"}
+      type={"text"}
+      title={"Message Box Title"}
+      text="Here is a text type message box"
+    />
     </div>
   );
 }
